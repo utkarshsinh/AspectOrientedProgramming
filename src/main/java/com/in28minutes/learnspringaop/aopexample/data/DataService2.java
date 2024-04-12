eb6c0a5f694436df18fd525b4ddce9c73bf17181
utkarshsinh <usinh002@ucr.edu>
2024-04-12 01:02:09 -0400

    First Commit

---
+++ src/main/java/com/in28minutes/learnspringaop/aopexample/data/DataService2.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learnspringaop.aopexample.data;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DataService2 {
+	
+	public int[] retrieveData() {
+		
+		try {
+			Thread.sleep(30);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return new int[] {111, 222, 333, 444, 555};
+	}
+
+}
